34035f3b2d1cff8f50f3218e5f4c18c177f388b8
Gazyn <rumblefighter2010@gmail.com>
2018-06-12 04:32:52 +0300

    Removed sin

--- powercalc/script.js
+++ powercalc/script.js
@@ -1,37 +1,41 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     //////////
-    var exponents = ["K", "M", "B", "T", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "aa", "ab", "ac", "ad", "ae", "af", "ag", "ah", "ai", "aj", "ak", "al", "am", "an", "ao", "ap", "aq", "ar", "as", "at", "au", "av", "aw", "ax", "ay", "az", "ba", "bb", "bc", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bk", "bl", "bm", "bn", "bo", "bp", "bq", "br", "bs", "bt", "bu", "bv", "bw", "bx", "by", "bz", "Legitamite? What does that mean? Is that like a chemical compound or something?"];
-    tab1button.addEventListener("click", function() {
+    var exponents = ["K", "M", "B", "T", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "aa", "ab", "ac", "ad", "ae", "af", "ag", "ah", "ai", "aj", "ak", "al", "am", "an", "ao", "ap", "aq", "ar", "as", "at", "au", "av", "aw", "ax", "ay", "az", "ba", "bb", "bc", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bk", "bl", "bm", "bn", "bo", "bp", "bq", "br", "bs", "bt", "bu", "bv", "bw", "bx", "by", "bz"];
+	tab1button.addEventListener("click", function() {
         tab1.style.visibility = "visible";
         tab2.style.visibility = "hidden";
         tab3.style.visibility = "hidden";
+		tab4.style.visibility = "hidden";
     });
     tab2button.addEventListener("click", function() {
         tab1.style.visibility = "hidden";
         tab2.style.visibility = "visible";
         tab3.style.visibility = "hidden";
+		tab4.style.visibility = "hidden";
     });
     tab3button.addEventListener("click", function() {
         tab1.style.visibility = "hidden";
         tab2.style.visibility = "hidden";
         tab3.style.visibility = "visible";
+		tab4.style.visibility = "hidden";
     });
-    //////////
+	tab4button.addEventListener("click", function() {
+		tab1.style.visibility = "hidden";
+		tab2.style.visibility = "hidden";
+		tab3.style.visibility = "hidden";
+		tab4.style.visibility = "visible";
+	});
+	//////////
     function updateawakening1() {
         if (input1.value > 70) {
             input1.value = 70;
         }
-        var level = input1.value;
-        var ghostlevel = level;
-        var total = level * 10 + level * (level - 1);
-        var lasttotal = total - (level + 4) * 2;
-        var displaytotal = Math.floor(total);
-        var displaylasttotal = Math.floor(lasttotal);
-        var exp = Math.floor((total / lasttotal - 1) * 10000) / 100;
-        eff1.textContent = ("Effect: " + displaytotal + "%");
-        if (level > 1) {
-            lasteff1.textContent = ("Last level's effect: " + displaylasttotal + "%");
-            rel1.textContent = ("Relative increase from last level: " + exp + "%");
+        var total = input1.value * 10 + input1.value * (input1.value - 1);
+        var lasttotal = total-((input1.value-1)*2+10);
+        eff1.textContent = ("Effect: " + Math.floor(total) + "%");
+        if (input1.value > 1) {
+            lasteff1.textContent = ("Last level's effect: " + Math.floor(lasttotal) + "%");
+            rel1.textContent = ("Relative increase from last level: " + ((total / lasttotal - 1) * 100).toPrecision(3) + "%");
         } else {
             lasteff1.textContent = ("Last level's effect: N/A");
             rel1.textContent = ("Relative increase from last level: N/A");
@@ -43,18 +47,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (input2.value > 30) {
             input2.value = 30;
         }
-        var level = input2.value;
-        var ghostlevel = level;
-        var curr = 10;
-        var total = level * 10 + level * (level - 1);
-        var lasttotal = total - (level + 4) * 2;
-        var displaytotal = Math.floor(total);
-        var displaylasttotal = Math.floor(lasttotal);
-        var exp = Math.floor((total / lasttotal - 1) * 10000) / 100;
-        eff2.textContent = ("Effect: " + displaytotal + "%");
-        if (level > 1) {
-            lasteff2.textContent = ("Last level's effect: " + displaylasttotal + "%");
-            rel2.textContent = ("Relative increase from last level: " + exp + "%");
+        var total = input2.value * 10 + input2.value * (input2.value - 1);
+        var lasttotal = total-((input2.value-1)*2+10);
+        eff2.textContent = ("Effect: " + Math.floor(total) + "%");
+        if (input2.value > 1) {
+            lasteff2.textContent = ("Last level's effect: " + Math.floor(lasttotal) + "%");
+            rel2.textContent = ("Relative increase from last level: " + ((total / lasttotal - 1) * 100).toPrecision(3) + "%");
         } else {
             lasteff2.textContent = ("Last level's effect: N/A");
             rel2.textContent = ("Relative increase from last level: N/A");
@@ -75,24 +73,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
         } else if (ghostlevel > 20) {
             total -= 0.4;
             ghostlevel -= 20;
-        }
-        while (ghostlevel > 0) {
-            if (level <= 20) {
-                total -= 0.02;
-                ghostlevel -= 1;
-            } else {
-                total -= 0.01;
-                ghostlevel -= 1;
-            }
-        }
-        if (level < 20) {
+			total -= ghostlevel * 0.01;
+			ghostlevel = 0;
+        } else {
+			total -= ghostlevel * 0.02;
+			ghostlevel = 0;
+		}
+        if (level <= 20) {
             var lasttotal = total + 0.02;
         } else {
             var lasttotal = total + 0.01;
         }
-        var displaytotal = 100 - (Math.floor(total * 10000) / 100);
-        var displaylasttotal = 100 - (Math.floor(lasttotal * 10000) / 100);
-        var exp = Math.floor(lasttotal / total * 10000 - 10000) / 100;
+        var displaytotal = 100 - (total * 100).toPrecision(3);
+        var displaylasttotal = 100 - (lasttotal * 100).toPrecision(3);
+        var exp = (lasttotal / total * 100 - 100).toPrecision(3);
         eff3.textContent = ("Effect: -" + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff3.textContent = ("Last level's Effect: -" + Math.round(displaylasttotal) + "%");
@@ -117,29 +111,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
         } else if (ghostlevel > 75) {
             total -= 0.8;
             ghostlevel -= 75;
-            while (ghostlevel > 0) {
-                total -= 0.0025;
-                ghostlevel -= 1;
-            }
+            total -= ghostlevel * 0.0025;
+            ghostlevel = 0;
         } else if (ghostlevel > 45) {
             total -= 0.65;
             ghostlevel -= 45;
-            while (ghostlevel > 0) {
-                total -= 0.005;
-                ghostlevel -= 1;
-            }
+            total -= ghostlevel * 0.005;
+            ghostlevel = 0;
         } else if (ghostlevel > 20) {
             total -= 0.4;
             ghostlevel -= 20;
-            while (ghostlevel > 0) {
-                total -= 0.01;
-                ghostlevel -= 1;
-            }
+            total -= ghostlevel * 0.01;
+            ghostlevel = 0;
         } else if (ghostlevel <= 20) {
-            while (ghostlevel > 0) {
-                total -= 0.02;
-                ghostlevel -= 1;
-            }
+            total -= ghostlevel * 0.02;
+            ghostlevel = 0;
         }
         if (level > 75) {
             var lasttotal = total + 0.0025;
@@ -171,15 +157,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (ghostlevel >= 200) {
             total += 3000;
             ghostlevel -= 200;
-            while (ghostlevel > 0) {
-                total += 10;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 10;
+			ghostlevel = 0;
         } else {
-            while (ghostlevel > 0) {
-                total += 15;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 15;
+			ghostlevel = 0;
         }
         if (level > 200) {
             var lasttotal = total - 10;
@@ -188,12 +170,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         var displaytotal = Math.floor(total);
         var displaylasttotal = Math.floor(lasttotal);
-        var exp = (total / lasttotal * 100 - 100);
-        if (exp < 1) {
-            var exp = exp.toFixed(Math.abs(Math.log(exp) / Math.log(10)) + 3);
-        } else {
-            var exp = exp.toPrecision(3);
-        }
+        var exp = (total / lasttotal * 100 - 100).toPrecision(3);
         eff5.textContent = ("Effect: " + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff5.textContent = ("Last level's Effect: " + Math.round(displaylasttotal) + "%");
@@ -212,15 +189,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (ghostlevel >= 100) {
             total += 1000;
             ghostlevel -= 100;
-            while (ghostlevel > 0) {
-                total += 5;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 5;
+			ghostlevel = 0;
         } else {
-            while (ghostlevel > 0) {
-                total += 10;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 10;
+			ghostlevel = 0;
         }
         if (level > 100) {
             var lasttotal = total - 5;
@@ -229,12 +202,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         var displaytotal = Math.floor(total);
         var displaylasttotal = Math.floor(lasttotal);
-        var exp = (total / lasttotal * 100 - 100);
-        if (exp < 1) {
-            var exp = exp.toFixed(Math.abs(Math.log(exp) / Math.log(10)) + 3);
-        } else {
-            var exp = exp.toPrecision(3);
-        }
+        var exp = (total / lasttotal * 100 - 100).toPrecision(3);
         eff6.textContent = ("Effect: " + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff6.textContent = ("Last level's Effect: " + Math.round(displaylasttotal) + "%");
@@ -253,15 +221,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (ghostlevel >= 500) {
             total += 5000;
             ghostlevel -= 500;
-            while (ghostlevel > 0) {
-                total += 2;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 2;
+			ghostlevel = 0;
         } else {
-            while (ghostlevel > 0) {
-                total += 10;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 10;
+			ghostlevel = 0;
         }
         if (level > 500) {
             var lasttotal = total - 2;
@@ -270,12 +234,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         var displaytotal = Math.floor(total);
         var displaylasttotal = Math.floor(lasttotal);
-        var exp = (total / lasttotal * 100 - 100);
-        if (exp < 1) {
-            var exp = exp.toFixed(Math.abs(Math.log(exp) / Math.log(10)) + 3);
-        } else {
-            var exp = exp.toPrecision(3);
-        }
+        var exp = (total / lasttotal * 100 - 100).toPrecision(3);
         eff7.textContent = ("Effect: " + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff7.textContent = ("Last level's Effect: " + Math.round(displaylasttotal) + "%");
@@ -294,15 +253,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (ghostlevel >= 100) {
             total += 1000;
             ghostlevel -= 100;
-            while (ghostlevel > 0) {
-                total += 5;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 5;
+			ghostlevel = 0;
         } else {
-            while (ghostlevel > 0) {
-                total += 10;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 10;
+			ghostlevel = 0;
         }
         if (level > 100) {
             var lasttotal = total - 5;
@@ -311,12 +266,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         var displaytotal = Math.floor(total);
         var displaylasttotal = Math.floor(lasttotal);
-        var exp = (total / lasttotal * 100 - 100);
-        if (exp < 1) {
-            var exp = exp.toFixed(Math.abs(Math.log(exp) / Math.log(10)) + 3);
-        } else {
-            var exp = exp.toPrecision(3);
-        }
+        var exp = (total / lasttotal * 100 - 100).toPrecision(3);
         eff8.textContent = ("Effect: " + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff8.textContent = ("Last level's Effect: " + Math.round(displaylasttotal) + "%");
@@ -341,15 +291,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         } else if (ghostlevel > 200) {
             total += 20;
             ghostlevel -= 200;
-            while (ghostlevel > 0) {
-                total += 0.02;
-                ghostlevel -= 1;
-            }
-        } else
-            while (ghostlevel > 0) {
-                total += 0.1;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 0.02;
+			ghostlevel = 0;
+        } else {
+            total += ghostlevel * 0.1;
+			ghostlevel = 0;
+		}
         if (level > 200) {
             var lasttotal = total - 0.02;
         } else {
@@ -357,7 +304,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         var displaytotal = Math.round(total * 100);
         var displaylasttotal = Math.round(lasttotal * 100);
-        var exp = Math.floor(total / lasttotal * 10000 - 10000) / 100
+        var exp = (total / lasttotal * 100 - 100).toPrecision(3);
         eff9.textContent = ("Effect: " + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff9.textContent = ("Last level's Effect: " + Math.round(displaylasttotal) + "%");
@@ -382,15 +329,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
         } else if (ghostlevel > 100) {
             total += 5;
             ghostlevel -= 100;
-            while (ghostlevel > 0) {
-                total += 0.02;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 0.02;
+			ghostlevel = 0;
         } else
-            while (ghostlevel > 0) {
-                total += 0.05;
-                ghostlevel -= 1;
-            }
+            total += ghostlevel * 0.05;
+			ghostlevel = 0;
         if (level > 100) {
             var lasttotal = total - 0.02;
         } else {
@@ -398,7 +341,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
         var displaytotal = Math.round(total * 100);
         var displaylasttotal = Math.round(lasttotal * 100);
-        var exp = Math.floor(total / lasttotal * 10000 - 10000) / 100
+        var exp = (total / lasttotal * 100 - 100).toPrecision(3);
         eff10.textContent = ("Effect: " + Math.round(displaytotal) + "%");
         if (level > 1) {
             lasteff10.textContent = ("Last level's Effect: " + Math.round(displaylasttotal) + "%");
@@ -410,8 +353,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
         return total + 1;
     }
     //////////
-    setInterval(function() {
-        var baseattack = baseattackinput.value / 100 + 1;
+	function updatePower() {
+		var baseattack = baseattackinput.value / 100 + 1;
         var awakening1power = updateawakening1();
         var awakening2power = updateawakening2();
         var mithrilswordpower = updatemithrilsword();
@@ -424,15 +367,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var coatofgoldpower = updatecoatofgold();
         var totalpower = baseattack * awakening1power * awakening2power * mithrilswordpower * mistilteinnpower * goldvesselspower * halberdpower * philostonepower * goldenglovespower * caduceuspower * coatofgoldpower;
         totalpowerdisplay.textContent = ("Total Power: " + totalpower.toPrecision(3));
-        if (!powerinput.value == "") {
+	}
+    function updateFloor() {
+		if (!powerinput.value == "") {
             var log = 1 + (1.585 + topfloorinput.value * 0.001117) / 100
             var lowestimate = Math.round(Math.log(powerinput.value) / Math.log(log) * 0.98 / 5) * 5;
             var highestimate = Math.round(Math.log(powerinput.value) / Math.log(log) * 1.02 / 5) * 5;
-            console.log(log);
-            console.log((lowestimate + highestimate) / 2)
             floorestimateoutput.textContent = lowestimate + " - " + highestimate;
         }
-        if (!goldboxinput.value == "") {
+	}
+	function updateGoldBox() {
+		if (!goldboxinput.value == "") {
             var level = goldboxinput.value;
             var tiergoal = 10
             var tier = 1;
@@ -448,17 +393,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     five = true;
                 }
             }
-            //Get gold amount
             var result = Math.pow(4, tier - 1) * Math.pow(level, 2) + Math.pow(4, tier) * level;
             if (result > 1e250) {
                 var exponent = exponents[82];
             } else {
-                //Get appropriate exponent (K, M, B, etc)
                 var exponent = exponents[Math.floor((Math.floor(Math.log(result) / Math.log(10))) / 3) - 1];
             };
-            //Get the amount of gold while considering exponents??
             var resultnoexpo = (result / Math.pow(10, ((exponents.indexOf(exponent) + 1) * 3))).toFixed(3);
-            //Previous returns a float. Replace the decimal with an exponent
             var decimalposition = resultnoexpo.toString().indexOf(".");
             if (result >= 1e3) {
                 var finalresult = "";
@@ -472,12 +413,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 if (result >= 1e6) {
                     finalresult = finalresult + exponents[exponents.indexOf(exponent) - 1];
                 }
-            //Ignore all this when theres no exponent
             } else {
                 var finalresult = result;
             }
-            //Find out how this is (probably) VERY broken in the morning.
             goldboxoutput.textContent = finalresult + " gold on new dungeon";
         }
+	}
+	function parseAttack() {
+		var input=expin.value;
+		if (!input == "") {
+			for(var i=exponents.length; i>0; i-=1) {
+				if(!input.indexOf(exponents[i-1])==-1) {
+					console.log(exponents[i-1]);
+				}
+			}
+		}
+	}
+	setInterval(function() {
+        parseAttack();
+		updatePower();
+        updateFloor();
+		updateGoldBox();
     }, 200);
 });
